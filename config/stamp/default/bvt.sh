d209714e60d8c6e918fba5b19b69257ddf424860
chenyutong84 <chenyutong84@126.com>
2018-04-23 15:22:41 +0800

    1

--- config/stamp/default/bvt.sh
+++ config/stamp/default/bvt.sh
@@ -16,7 +16,8 @@ ENVIRONMENT=bvt
 ##########################
 
 # the site certificate files (.crt & .key) have to be named after the BASE_URL value
-BASE_URL=trafficmanager.cn 
+# BASE_URL=trafficmanager.cn 
+BASE_URL=allenficusOxa.com
 LMS_URL=lms.$BASE_URL
 CMS_URL=cms.$BASE_URL
 PREVIEW_URL=preview.$BASE_URL

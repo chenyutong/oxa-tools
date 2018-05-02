d8acdfa0454c26e8d2717c9e9f4b26250d6b2872
chenyutong84 <chenyutong84@126.com>
2018-05-02 17:29:27 +0800

    修改baseurl

--- config/stamp/default/bvt.sh
+++ config/stamp/default/bvt.sh
@@ -17,9 +17,9 @@ ENVIRONMENT=bvt
 
 # the site certificate files (.crt & .key) have to be named after the BASE_URL value
 # BASE_URL=trafficmanager.cn 
-BASE_URL=lms.tmooc.cn
+BASE_URL=tmooc.cn 
 LMS_URL=lms.$BASE_URL
-CMS_URL=cms.$BASE_URL
+CMS_URL=lmsconsole.$BASE_URL
 PREVIEW_URL=preview.$BASE_URL
 
 # deployment configuration
